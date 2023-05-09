35472df30ef6f754966d284e1d00422b12dd70f5
mmmrsk <f55651068@gmail.com>
2023-05-09 18:11:38 +0300

    fix for pon and BunTest

--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -19,6 +19,18 @@ public class BunTest {
     @Parameterized.Parameters(name = "Bun {index} -> Name: {0}, Price:{1}")
     public static Object[][] bunTestData() {
         return new Object[][]{
+                {"", 300},
+                {null, 200},
+                {"       ", 200},
+                {"другой язык булки", 200},
+                {"1234567890", 200},
+                {"a", 200},
+                {"this is name of very delicious bun with very very long name", 100},
+                {"(black bun)&*^%$#@!_", 200},
+                {"black bun", -1},
+                {"white bun", 0},
+                {"red bun", Float.MIN_VALUE},
+                {"black bun", Float.MAX_VALUE},
                 {"black bun", 100},
                 {"white bun", 200},
                 {"red bun", 300}
